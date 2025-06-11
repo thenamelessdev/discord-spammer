686255359d0b32810bf589212ee9cb6436d2665f
thenamelessdev <aron.minarovits@gmail.com>
2025-06-11 08:17:37 +0200

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import time
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -8,8 +9,6 @@ file_path = os.path.join(script_dir, "token.txt")
 with open(file_path, "r") as file:
     token = file.read()
 
-channelid = input("Write the target channel ID here: ")
-message = input("What should be the message? Type it here: ")
 
 def send_message():
     headers = {
@@ -21,5 +20,25 @@ def send_message():
     request = requests.post(f"https://discord.com/api/v10/channels/{channelid}/messages", headers=headers, json=body)
     print(request)
 
-while True:
-    send_message()
+def jointhenleave():
+    headers = {
+        "Authorization": token
+    }
+
+    join = requests.post(f"https://discord.com/api/invites/{invite}", headers=headers)
+    print(join)
+    time.sleep(0.5)
+    leave = requests.delete(f"https://discord.com/api/invites/{invite}", headers=headers)
+    
+
+spammer = input("What spammer would you like to use? 1: Message spammer 2: Join-then-leave spammer")
+
+if spammer == "1": 
+    channelid = input("Write the target channel ID here: ")
+    message = input("What should be the message? Type it here: ")
+    while True:
+        send_message()
+elif spammer == "2":
+    invite = input("Put the server invite code here: ")
+    while True:
+        jointhenleave()
